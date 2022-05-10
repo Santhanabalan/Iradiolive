173174f05453c1cec1a1b2242568cddb4fb066a3
Santhanabalan <bala2102002@gmail.com>
2022-05-10 21:00:28 +0530

    Style & CDN Update
    --> CDN
    --> STYLE IMPROVEMENTS

---
+++ assets/js/player.js
@@ -0,0 +1,11 @@
+Amplitude.init({
+    songs: [
+        {
+            "name": "Song Name 1",
+            "artist": "Artist Name",
+            "album": "Album Name",
+            "url": "/../song/test.mp3",
+            "cover_art_url": "/../img/logo.png"
+        },
+    ]
+});
